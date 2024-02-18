9157db8c78f83bc14eefd679a6795b9ffc98b396
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2024-02-18 20:28:15 -0300

    feat: add init loop

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 int** matrix_2d_create_empty(int row_size, int col_size){
     int** matrix = (int**)calloc(row_size, sizeof(int*));
@@ -69,46 +70,47 @@ void copy_matrix(int** from, int** dest, int row, int col){
 int main(void){
     const int size = 5;
     int** matrix = matrix_2d_create_empty(size, size);
-    int** new_matrix = matrix_2d_create_empty(size, size);
+    int** temp_matrix = matrix_2d_create_empty(size, size);
 
     // blinker seed
     matrix[2][1] = 1;
     matrix[2][2] = 1;
     matrix[2][3] = 1;
 
-    int dump = count_neighbors(matrix, 2, 3, 1);
-    printf("numf of life: %d\n",dump);
-
-    copy_matrix(matrix, new_matrix, size, size);
+    copy_matrix(matrix, temp_matrix, size, size);
     matrix_2d_print(matrix, size, size);
     
     int live_cell = 1;
     int dead_cell = 0;
-    // execute rules
-    for(int i = 1; i < size-1; ++i){
-        for(int j = 1; j < size-1; ++j){
-            int nums_of_live_neighbors = count_neighbors(matrix, i, j, live_cell);
-            bool live_cell_dies = matrix[i][j] == live_cell && (nums_of_live_neighbors < 2 || nums_of_live_neighbors > 3);
-            if(live_cell_dies){
-                new_matrix[i][j] = 0;
-                continue;
-            }
-            
-            bool dies_cell_can_born = matrix[i][j] == dead_cell && nums_of_live_neighbors == 3;
-            if(dies_cell_can_born){
-                new_matrix[i][j] = 1;
-                continue;
+    while(true){
+        // execute rules
+        for(int i = 1; i < size-1; ++i){
+            for(int j = 1; j < size-1; ++j){
+                int nums_of_live_neighbors = count_neighbors(matrix, i, j, live_cell);
+                bool live_cell_dies = matrix[i][j] == live_cell && (nums_of_live_neighbors < 2 || nums_of_live_neighbors > 3);
+                if(live_cell_dies){
+                    temp_matrix[i][j] = 0;
+                    continue;
+                }
+                
+                bool dies_cell_can_born = matrix[i][j] == dead_cell && nums_of_live_neighbors == 3;
+                if(dies_cell_can_born){
+                    temp_matrix[i][j] = 1;
+                    continue;
+                }
             }
         }
+
+        copy_matrix(temp_matrix, matrix, size, size);
+        matrix_2d_print(matrix, size, size);
+        printf("\033[H\033[J");
+        sleep(1);
     }
-
-    matrix_2d_print(new_matrix, size, size);
-
     // die: live cell < 2 live neigh or > 3 live neigh
     // live: live cell == 2 or 3 live neigh
     // born: cell == 3 live negh
     
     matrix_2d_free(matrix, size);
-    matrix_2d_free(new_matrix, size);
+    matrix_2d_free(temp_matrix, size);
     return 0;
 }
